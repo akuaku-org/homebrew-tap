f47e8b8ad2a0c34d77148d665ed0e063fba2fc2f
Dav Yaginuma <dav@akuaku.org>
2026-06-10 16:40:05 -0700

    vitrine-agent-pipe 0.1.0

--- Formula/vitrine-agent-pipe.rb
+++ Formula/vitrine-agent-pipe.rb
@@ -2,7 +2,7 @@ class VitrineAgentPipe < Formula
   desc "Bridge Vision Pro app text queries into a Mac agent's tmux session"
   homepage "https://akuaku.org/vitrine/"
   url "https://akuaku.org/vitrine/vitrine-agent-pipe-0.1.0-universal.tar.gz"
-  sha256 "03fa48d14aff1893f387c95ec8aa2b82d61817dc9d371404aaffab5822925a78"
+  sha256 "f596e2f272fcf83f69a3ae59e1bc3518ffe43d496bc8a8d658709a0e9b18ecb5"
   version "0.1.0"
 
   def install
